888f0e6ba5b32978e989b4f45365b1dbcca9352f
Nalipp <nateplipp@gmail.com>
2017-08-16 11:11:15 +0900

    initial commit

---
+++ buildScripts/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log('up and running on : ' + port);
+});
